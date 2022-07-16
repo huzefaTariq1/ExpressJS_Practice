b3e6ae0475fcd1e32937fd41b4f9d2333f7002d6
HuzefaTariq <101399969+huzefaTariq1@users.noreply.github.com>
2022-07-16 20:27:54 +0500

    input validation using Npm Joi

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express")
+const Joi=require("joi")
 const app = express();
 
 app.use(express.json())
@@ -30,6 +31,19 @@ app.get("/api/courses/:id",(req,res)=>{
 })
 
 app.post('/api/courses',(req,res)=>{
+   
+    // defining schema which will works on validation
+   const schema={
+    name:Joi.string().min(3).required()
+   } 
+  // storing joi validation result
+   const result= Joi.validate(req.body,schema)
+
+  if(result.error){
+    //400 bad request
+    res.status(400).send(result.error.details[0].message)
+  }
+
     const course={
         id:courses.length+1,
         course:req.body.name
